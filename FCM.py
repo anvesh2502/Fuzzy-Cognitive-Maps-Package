2a443783d36e8a435ed2ca1d4780ba362c24dd1f
anvesh2502 <anveshrocks@yahoo.co.in>
2016-10-06 18:03:15 -0500

    Adding the package file

---
+++ FCM.py
@@ -0,0 +1,17 @@
+import networkx as nx
+
+
+'''
+This is a Python package for Fuzzy Cognitive Maps
+
+'''
+
+
+class FCM :
+
+    def __init__(self) :
+
+        self._fcm_graph=nx.DiGraph()
+
+
+       
